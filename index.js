b7e56b5deb80c55350e473d678f5a713280cfdcc
Mohamed Eldfrawy <mohmmedeldfrawy@gmail.com>
2026-04-13 18:36:43 +0200

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from   'express'
 const app = express()
-const port = 3000
+const port =  process.env.port|| 3000
 import {dbconnect} from '../Management API/db/db.js'
 // import {Get_All,Get_single,updata_product,deleted_product} from './modules/products/routes.js'
 app.use(express.json());
